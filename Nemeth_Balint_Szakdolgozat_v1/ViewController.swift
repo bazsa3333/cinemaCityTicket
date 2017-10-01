639fabfc7886d086b0311bec15886c809af81207
bazsa3333 <bazsa3333@gmail.com>
2017-10-01 15:20:10 +0200

    Initial Commit

---
+++ Nemeth_Balint_Szakdolgozat_v1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nemeth_Balint_Szakdolgozat_v1
+//
+//  Created by Bálint Németh on 2017. 10. 01..
+//  Copyright © 2017. Németh Bálint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
